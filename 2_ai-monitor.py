2cac18a16a1f6f45dd6ac9e83d8158a415def247
plemaist <plemaist@cisco.com>
2024-07-11 09:28:59 -0500

    update

--- 2_ai-monitor.py
+++ 2_ai-monitor.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import subprocess
 import time
 
@@ -28,17 +26,13 @@ def get_cpu_average():
 
 def get_gpu_info():
     command = "nvidia-smi --query-gpu=gpu_name,memory.used,utilization.gpu,memory.total --format=csv,noheader,nounits"
-    try:
-        result = subprocess.check_output(command, shell=True).decode().strip()
-    except subprocess.CalledProcessError:
-        return 'N/A', 0.0, 0.0, 0.0
-    else:
-        gpu_name, memory_used, gpu_utilization, gpu_memory = result.split(", ")
-        memory_used = int(memory_used)
-        gpu_memory = int(gpu_memory)
-        gpu_memory_gib = round(gpu_memory / 1024, 2)
-        memory_used = round(memory_used / 1024, 2)
-        return gpu_name, memory_used, gpu_utilization, gpu_memory_gib
+    result = subprocess.check_output(command, shell=True).decode().strip()
+    gpu_name, memory_used, gpu_utilization, gpu_memory = result.split(", ")
+    memory_used = int(memory_used)
+    gpu_memory = int(gpu_memory)
+    gpu_memory_gib = round(gpu_memory / 1024, 2)
+    memory_used = round(memory_used / 1024, 2)
+    return gpu_name, memory_used, gpu_utilization, gpu_memory_gib
 
 def get_memory_info():
     command = "free -h | awk '/^Mem:/ {print $2, $3, $4}'"
@@ -46,24 +40,25 @@ def get_memory_info():
     total, used, available = result.split()
     return total, used, available
 
-def main():
-    cpu_sockets = get_cpu_sockets()
-    cpu_cores = get_cpu_cores()
-    cpu_type = get_cpu_type()
-    gpu_name, memory_used, gpu_utilization, gpu_memory_gib = get_gpu_info()
-    print(f"\nCisco UCSX-210C-M6 computing node with X440p PCIE node X-Fabric Enabled")
-    print(f"\nCPU Type: {cpu_sockets} x {cpu_type} with {cpu_cores} cores each")
-    print(f"GPU Type: {gpu_name}\n")
-    print(f"\nCPU util       CPU mem used/total       GPU mem used/total       GPU util")
-    while True:
+cpu_sockets = get_cpu_sockets()
+cpu_cores = get_cpu_cores()
+cpu_type = get_cpu_type()
+gpu_name, memory_used, gpu_utilization, gpu_memory_gib = get_gpu_info()
+print(f"\nCisco UCSX-210C-M6 computing node with X440p PCIE node X-Fabric Enabled")
+print(f"\nCPU Type: {cpu_sockets} x {cpu_type} with {cpu_cores} cores each")
+print(f"GPU Type: {gpu_name}\n")
+print(f"\nCPU util       CPU mem used/total       GPU mem used/total       GPU util")
+while True:
+    if check_for_gpu()
+        cpu_average = get_cpu_average()
+        total_memory, used_memory, available_memory = get_memory_info()
+
+        print(f"\r {cpu_average}%          {used_memory}/{total_memory}              No GPU           No GPU    ", end="", flush=True)
+        time.sleep(1)
+    else:
         cpu_average = get_cpu_average()
         gpu_name, memory_used, gpu_utilization, gpu_memory_gib = get_gpu_info()
         total_memory, used_memory, available_memory = get_memory_info()
 
         print(f"\r {cpu_average}%          {used_memory}/{total_memory}              {memory_used}Gi/{gpu_memory_gib}Gi           {gpu_utilization}%    ", end="", flush=True)
         time.sleep(1)
-
-try:
-    main()
-except KeyboardInterrupt:
-    sys.exit()
